01c6a766ec77797b22fd91060ece337840763bba
qzh <2806894439@qq.com>
2023-12-31 21:27:09 +0800

    打印心形的第二种方式

---
+++ 01-Java语言概述/src/Exercise3.java
@@ -0,0 +1,16 @@
+/**
+ * 输出心形方式二
+ */
+public class Exercise3 {
+    public static void main(String[] args) {
+        System.out.print("\t"+"*"+"\t\t\t\t\t\t\t\t\t\t\t\t"+"*"+"\t"+"\n");
+        System.out.print("*"+"\t\t"+"*"+"\t\t\t\t"+"I love Java"+"\t\t\t\t"+"*"+"\t\t"+"*"+"\n");
+        System.out.print("\t"+"*"+"\t\t\t\t\t\t\t\t\t\t\t\t"+"*"+"\t"+"\n");
+        System.out.print("\t\t"+"*"+"\t\t\t\t\t\t\t\t\t\t"+"*"+"\t\t"+"\n");
+        System.out.print("\t\t\t"+"*"+"\t\t\t\t\t\t\t\t"+"*"+"\t"+"\n");
+        System.out.print("\t\t\t\t"+"*"+"\t\t\t\t\t\t"+"*"+""+"\t"+"\n");
+        System.out.print("\t\t\t\t\t"+"*"+"\t\t\t\t"+"*"+""+"\t\t"+"\n");
+        System.out.print("\t\t\t\t\t\t"+"*"+"\t\t"+"*"+""+"\t\t"+"\n");
+        System.out.print("\t\t\t\t\t\t\t"+"*"+"\n");
+    }
+}
